7b090e867a68b9edd2d390fa590223fba934dc07
ghrbs0437 <42041887+ghrbs0437@users.noreply.github.com>
2024-05-16 16:40:34 +0900

    [level 2] Title: 더 맵게, Time: 3736.45 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i:scoville){
+            pq.add(i);
+        }
+        
+        while(true){    
+            if(pq.peek()>=K){
+                return answer;
+            }
+            if(pq.size()<2){
+                break;
+            }
+            answer++;
+            int min = pq.peek();
+            pq.remove();
+            int min2 = pq.peek();
+            pq.remove();
+            pq.add(min + min2*2);
+        }
+        
+        if(pq.peek()>=K){
+            return answer;
+        }
+        
+        return -1;
+    }
+}
