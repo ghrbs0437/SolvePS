2de582d095143adc0bf30dd5a8d3890ec6e57994
ghrbs0437 <42041887+ghrbs0437@users.noreply.github.com>
2024-05-20 13:42:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.08 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        // 선행 작업이 끝난 시점에서, 작업이 들어왔던 것 중 작업요구량이 제일 적은것을 task할당
-        
         PriorityQueue<TaskJob> pq = new PriorityQueue<>();
         Arrays.sort(jobs,(a,b)->{
             if(a[0]!=b[0]){
@@ -15,12 +13,6 @@ class Solution {
         });
         pq.add(new TaskJob(jobs[0][0],jobs[0][1]));
         
-        // for(int[] job : jobs){
-        //     for(int j : job){
-        //         System.out.print(j+" ");
-        //     }
-        //     System.out.println("");
-        // }
         int index = 0;
         int currentTime = jobs[0][0];
         int answer = 0;
@@ -29,15 +21,12 @@ class Solution {
            
             // 1. 작업수행
             TaskJob tj = pq.peek();
-           
             currentTime += tj.runningTime;
-            // System.out.println(currentTime - tj.takeTime);
             answer += currentTime - tj.takeTime;
             pq.remove();
-            // 2. 작업동안 Task 할당
+            // 2. 이번 작업동안 추가로 들어온 Task
             for(int i=index+1; i<jobs.length;i++){
                 if(jobs[i][0] <= currentTime){
-                    // System.out.println(i);
                     pq.add(new TaskJob(jobs[i][0],jobs[i][1]));
                     index++;
                 }else{
@@ -45,12 +34,12 @@ class Solution {
                 }
             }
             
-            // 3. 작업이 끝난 후 Task 확인
+            // 3. 작업이 끝난 후 다음 Task 확인
             if(pq.size()==0){
                 if(index == jobs.length-1){
                     return answer/jobs.length;
                 }
-                else{
+                else{ // 현재 부여받은 Task가 없어도 끝이 아닐수 있음.
                     index++;
                     pq.add(new TaskJob(jobs[index][0],jobs[index][1]));
                     currentTime  = jobs[index][0];
