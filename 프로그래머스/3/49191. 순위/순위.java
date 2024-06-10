32c69d1b3c86eb0ef1e10f857db29c97fc5baf28
ghrbs0437 <42041887+ghrbs0437@users.noreply.github.com>
2024-06-10 15:22:00 +0900

    [level 3] Title: 순위, Time: 122.88 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] results) {
+        
+        int[][] players = new int[n][n];
+        
+        for(int[] result :results){
+            players[result[0]-1][result[1]-1] = 1;
+            players[result[1]-1][result[0]-1] = -1;
+        }
+        
+        
+        for(int i=0;i<n;i++){   
+            winPropagation(i,players,n);
+            losePropagation(i,players,n);
+        }
+        
+        // for(int[] a : players){
+        //     for(int i: a){
+        //         System.out.print(i+" ");
+        //     }
+        //     System.out.println("");
+        // }
+        
+        int answer = 0;
+        for(int i=0;i<n;i++){
+            int count = 0;
+            for(int j=0;j<n;j++){
+                if(players[i][j] !=0){
+                    count++;
+                }
+            }
+            if(count>=n-1){
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+//     public void propagation(int start, int[][] players,int number){
+//         for(int i=0;i<number;i++){
+//             if(players[start][i]!=0){
+//                 getCanWinList();
+//                 propagation(i,players,number);
+//             }
+//         }
+//     }
+    
+    public void winPropagation(int start, int[][] players,int number){
+        Queue<Integer> queue = new LinkedList<>();
+        HashSet<Integer> visit = new HashSet<>();
+        queue.add(start);
+        while(queue.size()!=0){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int current = queue.poll();
+                for(int j=0;j<number;j++){
+                    if(visit.contains(j)){
+                        continue;
+                    }
+                    
+                    if(players[current][j]==1){
+                        players[start][j] = 1;
+                        queue.add(j); 
+                    }
+                }
+                visit.add(current);
+            }
+        }
+    }
+    
+    public void losePropagation(int start, int[][] players,int number){
+        Queue<Integer> queue = new LinkedList<>();
+        HashSet<Integer> visit = new HashSet<>();
+        queue.add(start);
+        while(queue.size()!=0){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int current = queue.poll();
+                for(int j=0;j<number;j++){
+                    if(visit.contains(j)){
+                        continue;
+                    }
+                    if(players[current][j]==-1){
+                        players[start][j] = -1;
+                        queue.add(j); 
+                    }
+                }
+                visit.add(current);
+            }
+        }
+    }
+    
+
+}
