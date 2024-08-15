218eef2d828fcf8e1366ba6fc939a1d6030153ed
YHK <42041887+ghrbs0437@users.noreply.github.com>
2024-08-16 00:00:49 +0900

    [Gold IV] Title: 문자열 폭발, Time: 332 ms, Memory: 25476 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,34 +9,38 @@ public class Main {
         String str =br.readLine();
         String boomStr = br.readLine();
 
-
-        ArrayList<Character> alist = new ArrayList<>();
-
+        char[] newStr = new char[str.length()];
+        int newStrIndex = 0;
+        char lastBoomChar = boomStr.charAt(boomStr.length()-1);
         str :
         for(int i=0;i<str.length();i++){
             char c = str.charAt(i);
-            alist.add(c);
+            newStr[newStrIndex] = c;
+            newStrIndex++;
 
-            if(c==boomStr.charAt(boomStr.length()-1)){
+            if(newStrIndex<boomStr.length()){
+                continue;
+            }
+
+            if(c==lastBoomChar){
                 for(int j=0;j<boomStr.length();j++){
-                    if(alist.size()<boomStr.length()){
-                        continue str;
-                    }
-                    if(alist.get(alist.size()-1-j) != boomStr.charAt(boomStr.length()-1-j)){
+                    if(newStr[newStrIndex-1-j] != boomStr.charAt(boomStr.length()-1-j)){
                        continue str;
                     }
                 }
                 for(int j=0;j<boomStr.length();j++){
-                    alist.remove(alist.size()-1);
+                    newStr[newStrIndex-1] = 0;
+                    newStrIndex--;
                 }
             }
         }
+
         StringBuilder sb = new StringBuilder();
-        if(alist.isEmpty()){
+        if(newStrIndex==0){
             sb.append("FRULA");
         }else{
-            for(Character c : alist){
-                sb.append(c);
+            for(int i=0;i<newStrIndex;i++){
+                sb.append(newStr[i]);
             }
         }
         System.out.println(sb);
