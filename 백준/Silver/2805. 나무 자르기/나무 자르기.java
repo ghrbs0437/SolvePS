eaa6be10eb6f3319004cb081c9427703dafe9fff
YHK <42041887+ghrbs0437@users.noreply.github.com>
2024-08-22 01:00:49 +0900

    [Silver II] Title: 나무 자르기, Time: 488 ms, Memory: 137532 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -4,75 +4,55 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		
-		
-		String[] strs = br.readLine().split(" ");
-		
-		int N = Integer.parseInt(strs[0]);
-		int M = Integer.parseInt(strs[1]);
-		
-		strs = br.readLine().split(" ");
-		int[] arr = new int[N];
-		
-		int maxWoodLength = 0;
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(strs[i]);
-			if(maxWoodLength<arr[i]) {
-				maxWoodLength = arr[i];
-			}
-		}
-		
-		
-		int height = 0;
-		int dHeight = maxWoodLength;
-		
-		int answer = 0;
-		
-		HashSet<Integer> visits = new HashSet<>();
-		checkCycle : while(true) {
-			if(dHeight!=1) {
-				dHeight>>=1;
-			}else {
-				if(visits.contains(height)) {
-					break;
-				}
-			}
-//			System.out.println(height);
-			visits.add(height);
-			int sum = 0;
-			for(int i=0;i<N;i++) {
-				if(arr[i]<height) {
-					continue;
-				}
-				
-				sum+=arr[i]-height;
-				if(sum<0) { // 오버플로우 발생.. 톱의 높이를 높여라.
-					height += dHeight;
-					continue checkCycle;
-				}
+    public static void main(String[] args) throws IOException {
 
-//				최적화
-//				if(sum>=M) { // 톱의 높이를 높여라. 
-//					answer = height;
-//				}
-			}
-			if(sum<M) { // 높의 높이를 낮춰라.
-				height -= dHeight;
-			}else if(sum>=M){ // 톱의 높이를 높여라.
-//				System.out.println(height);
-				answer = Math.max(height, answer);
-				height += dHeight;
-			}
-		}
-		
-		System.out.println(answer);
-		
-		
-		
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strs = br.readLine().split(" ");
+
+        int N = Integer.parseInt(strs[0]);
+        int M = Integer.parseInt(strs[1]);
+
+        strs = br.readLine().split(" ");
+        int[] arr = new int[N];
+
+        int maxWoodLength = 0;
+        for(int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(strs[i]);
+            if(maxWoodLength<arr[i]) {
+                maxWoodLength = arr[i];
+            }
+        }
+
+        int min = 0;
+        int max = maxWoodLength;
+        int answer = 0;
+
+        checkCycle : while(min<=max) {
+            long height = ((long)max+(long)min)/2l;
+            int sum = 0;
+            for(int i=0;i<N;i++) {
+                if(arr[i]<height) {
+                    continue;
+                }
+                sum+=arr[i]-height;
+                if(sum<0) { // 오버플로우 발생.. 톱의 높이를 높여라.
+                    min = (int)height;
+                    continue checkCycle;
+                }
+            }
+            if(sum<M) { // 높의 높이를 낮춰라.
+                max = (int)height-1;
+            }else if(sum>=M){ // 톱의 높이를 높여라.
+                answer = Math.max((int)height, answer);
+                min = (int)height+1;
+            }
+        }
+
+        System.out.println(answer);
+
+
+
+    }
 }
