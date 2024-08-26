846ec2f1b4d5b8ff7d8db6f7548c11a0510538b7
YHK <42041887+ghrbs0437@users.noreply.github.com>
2024-08-27 00:14:48 +0900

    [Gold II] Title: 가스관, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Gold/2931. 가스관/가스관.java
+++ 백준/Gold/2931. 가스관/가스관.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class Main {
-    static int MAX_VALUE = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -18,26 +17,12 @@ public class Main {
         int ySize = Integer.parseInt(strs[0]);
         int xSize = Integer.parseInt(strs[1]);
 
-        int homeY = 0 ;
-        int homeX = 0;
-        int kdY = 0;
-        int kdX = 0;
 
         char[][] map = new char[ySize][xSize];
         for(int i=0;i<ySize;i++) {
             String str = br.readLine();
             for(int j=0;j<xSize;j++) {
                 map[i][j] = str.charAt(j);
-
-                if(map[i][j]=='M') {
-                    homeY = i;
-                    homeX = j;
-                }
-
-                if(map[i][j] =='Z') {
-                    kdY = i;
-                    kdX = j;
-                }
             }
         }
 
@@ -53,9 +38,6 @@ public class Main {
         hmap.put('3', new int[][]{{-1,0},{0,-1}});
         hmap.put('4', new int[][]{{1,0},{0,-1}});
 
-
-//		파이프중 아무데서나 시작해서 파이브를 따라 쭉 간다.. Z 나 M이나오면 그만. .
-
         boolean[][] visits = new boolean[ySize][xSize];
 
         ArrayList<int[]> answerList = getAns(visits,map,hmap);
@@ -67,34 +49,30 @@ public class Main {
             if(arr.length!=answerList.size()) {
                 continue;
             }
-
             for(int i=0;i<arr.length;i++) {
-                boolean okay = false;
+
+                boolean matched = false;
+
                 for(int[] direction : answerList) {
                     if(Arrays.equals(direction, arr[i])) {
-                        okay = true;
+                        matched = true;
                         // 같은게있으면 괜찮은데 없으면 continue해야해..
                     }
                 }
-                if(!okay) {
+                if(!matched) {
                     continue key;
                 }
             }
-            System.out.println((answerY+1)+" " +(answerX+1) + " "+ key);
+            sb.append(answerY+1).append(" ").append(answerX+1).append(" ").append(key);
+            System.out.println(sb);
             return;
         }
-
-
-        // 이아래는 일단 출발은 가능해..
 
     }
     static int answerY = 0;
     static int answerX = 0;
 
     public static ArrayList<int[]> getAns(boolean[][] visits,char[][] map, HashMap<Character, int[][]> hmap) {
-
-        int targetY = 0;
-        int targetX = 0;
 
         ArrayList<int[]> alist = new ArrayList<>();
 
@@ -117,17 +95,17 @@ public class Main {
                     if(ny<0 || nx<0 || ny>=map.length || nx >=map[0].length) {
                         continue;
                     }
+
                     if(map[ny][nx]=='.') {
                         answerY = ny;
                         answerX = nx;
-                        int[] rvdirec = {-direction[0],-direction[1]};
-                        alist.add(rvdirec);
+                        int[] answerDelta = {-direction[0],-direction[1]};
+                        alist.add(answerDelta);
                     }
                 }
 
             }
         }
-
         return alist;
     }
 }
