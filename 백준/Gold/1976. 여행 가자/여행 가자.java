5fbd1ff7cd303ddb9f4acbd521aea7415b292bd9
YHK <42041887+ghrbs0437@users.noreply.github.com>
2024-08-29 00:53:40 +0900

    [Gold IV] Title: 여행 가자, Time: 160 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[N][N];
+        for(int i=0;i<N;i++){
+            String[] strs = br.readLine().split(" ");
+            for(int j=0;j<N;j++){
+                map[i][j] = Integer.parseInt(strs[j]);
+            }
+        }
+
+        HashSet<Integer> tripPlan = new HashSet<>();
+        String [] strs = br.readLine().split(" ");
+        for(int i=0;i<M;i++){
+            tripPlan.add(Integer.parseInt(strs[i])-1);
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(Integer.parseInt(strs[0])-1);
+
+        boolean[] visits = new boolean[N];
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i=0;i<size;i++){
+                int current = queue.poll();
+                if(visits[current]){
+                    continue;
+                }
+                visits[current] = true;
+
+                for(int j=0;j<map.length;j++){
+                    if(map[current][j] == 1&&!visits[j]){
+                        queue.add(j);
+                    }
+                }
+            }
+        }
+        for(int trip : tripPlan){
+            if(!visits[trip]){
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+
+    }
+}
