3920a5f91873e340f693fbbd440811c1a1eb02c9
YHK <42041887+ghrbs0437@users.noreply.github.com>
2024-08-30 16:00:04 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 16616 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,129 +1,94 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class Main {
 
-    static int[][] directions = {{-1,0},{0,1},{1,0},{0,-1}};
+    static int [][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
 
-        char[][] arr = new char[size][size];
-        for(int i=0;i<size;i++){
+        StringBuilder sb = new StringBuilder();
+        int mapSize = Integer.parseInt(br.readLine());
+        char[][] map = new char[mapSize][mapSize];
+        for(int i=0;i<mapSize;i++){
             String str = br.readLine();
-            for(int j=0;j<size;j++){
-                arr[i][j] = str.charAt(j);
+            for(int j=0;j<str.length();j++){
+                map[i][j] = str.charAt(j);
             }
         }
 
-        boolean[][] normals = new boolean[size][size];
-        boolean[][] abNormals = new boolean[size][size];
 
-        int normalCnt = 0;
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                if(normals[i][j]){
+        int[][] section = new int[mapSize][mapSize];
+        int sectionIndex =1;
+        for(int i=0;i<mapSize;i++){
+            for(int j=0;j<mapSize;j++){
+                if(section[i][j]!=0){
                     continue;
                 }
-                normalCnt++;
-                // 방문한적이 없는거라면..
-                char c = arr[i][j];
-                normals[i][j] = true;
-
-                Queue<Integer> qx = new LinkedList<>();
-                Queue<Integer> qy = new LinkedList<>();
-                qx.add(j);
-                qy.add(i);
-                while(!qx.isEmpty()){
-                    int qSize = qx.size();
-                    for(int k=0;k<qSize;k++){
-                        int x = qx.poll();
-                        int y = qy.poll();
-                        normals[y][x] = true;
-                        for(int[] direction : directions){
-                            int dy = direction[0];
-                            int dx = direction[1];
-                            int nextX = x+dx;
-                            int nextY = y+dy;
-                            if(nextX==size||nextY==size||nextX<0||nextY<0){
-                                continue;
-                            }
-                            if(c!=arr[nextY][nextX]){
-                                continue;
-                            }
-                            if(normals[nextY][nextX]){
-                                continue;
-                            }
-                            qx.add(nextX);
-                            qy.add(nextY);
-                            normals[nextY][nextX] = true;
-                        }
-                    }
-                }
-
+                makeSection(map,new char[]{map[i][j]},section,i,j,sectionIndex);
+                sectionIndex++;
             }
         }
+        sb.append(sectionIndex-1+" ");
+        section = new int[mapSize][mapSize];
+        sectionIndex = 1;
+        for(int i=0;i<mapSize;i++){
+            for(int j=0;j<mapSize;j++){
 
-
-
-        int abNormalCnt = 0;
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                if(abNormals[i][j]){
+                if(section[i][j]!=0){
                     continue;
                 }
-                abNormalCnt++;
-                // 방문한적이 없는거라면..
-                char c = arr[i][j];
-                abNormals[i][j] = true;
-
-                Queue<Integer> qx = new LinkedList<>();
-                Queue<Integer> qy = new LinkedList<>();
-                qx.add(j);
-                qy.add(i);
-                while(!qx.isEmpty()){
-                    int qSize = qx.size();
-                    for(int k=0;k<qSize;k++){
-                        int x = qx.poll();
-                        int y = qy.poll();
-                        abNormals[y][x] = true;
-                        for(int[] direction : directions){
-                            int dy = direction[0];
-                            int dx = direction[1];
-                            int nextX = x+dx;
-                            int nextY = y+dy;
-                            if(nextX==size||nextY==size||nextX<0||nextY<0){
-                                continue;
-                            }
-                            if(abNormals[nextY][nextX]){
-                                continue;
-                            }
-
-                            char nextChar = arr[nextY][nextX];
-                            if(c!=nextChar){
-                                if( (c=='R' || c=='G') && (nextChar=='R' || nextChar=='G')){
-
-                                }else{
-                                    continue;
-                                }
-                            }
-                            qx.add(nextX);
-                            qy.add(nextY);
-                            abNormals[nextY][nextX] = true;
-                        }
-                    }
+                if(map[i][j] == 'R'|| map[i][j] =='G'){
+                    makeSection(map,new char[]{'R','G'},section,i,j,sectionIndex);
+                }else{
+                    makeSection(map,new char[]{'B'},section,i,j,sectionIndex);
                 }
-
+                sectionIndex++;
             }
         }
 
-        System.out.println(normalCnt+" "+abNormalCnt);
+        sb.append(sectionIndex-1);
+        System.out.println(sb);
 
     }
 
+    public static void makeSection(char[][] map,char[] sectionChar, int[][] section,int y,int x,int sectionIndex){
+        Queue<Integer> qx = new ArrayDeque<>();
+        Queue<Integer> qy = new ArrayDeque<>();
+        qx.add(x);
+        qy.add(y);
+        while(!qx.isEmpty()){
+            int size = qx.size();
+            for(int i=0;i<size;i++){
+                int cx = qx.poll();
+                int cy = qy.poll();
+
+                if(section[cy][cx]!=0){
+                    continue;
+                }
+                section[cy][cx] = sectionIndex;
+
+                for(int[] direction : directions){
+                    int dy = direction[0];
+                    int dx = direction[1];
+                    int ny = cy + dy;
+                    int nx = cx + dx;
+
+                    if(ny<0||nx<0||ny>=map.length||nx>=map[0].length){
+                        continue;
+                    }
+                    for(char c : sectionChar){
+                        if(map[ny][nx]==c){
+                            qx.add(nx);
+                            qy.add(ny);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
