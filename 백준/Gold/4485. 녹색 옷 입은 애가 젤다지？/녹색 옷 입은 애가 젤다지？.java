be1a85755993dbc587ede849bfba3c6ced7be9b5
YHK <42041887+ghrbs0437@users.noreply.github.com>
2024-09-04 17:28:55 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 180 ms, Memory: 20428 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -2,68 +2,67 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
-    static class Node{
-        int vx, vy, rupee;
-        public Node(int vx, int vy, int rupee) {
-            this.vx = vx;
-            this.vy = vy;
-            this.rupee = rupee;
-        }
-    }
-    private static int N;
-    private static int[][] cave;
-    private static int[][] dist;
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, -1, 0, 1};
 
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
+    public static int[][] map;
+    public static int[][] costMap;
     public static void main(String[] args) throws IOException {
-        int count = 0;
-        while(true) {
-            N = Integer.parseInt(br.readLine());
-            if(N == 0) break;
-            StringBuilder sb = new StringBuilder();
-            count++;
-            sb.append("Problem").append(" ").append(count).append(": ");
-            cave = new int[N][N];
-            dist = new int[N][N];
-
-            for (int i = 0; i < N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    cave[i][j] = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int tc = 0;
+        while(true){
+            tc++;
+            int size = Integer.parseInt(br.readLine());
+            if(size==0){
+                break;
+            }
+            map = new int[size][size];
+            costMap = new int[size][size];
+            for(int i=0;i<size;i++){
+                String[] strs = br.readLine().split(" ");
+                for(int j=0;j<size;j++){
+                    map[i][j] = Integer.parseInt(strs[j]);
+                    costMap[i][j] = Integer.MAX_VALUE;
                 }
             }
+            costMap[0][0] = map[0][0];
+            PriorityQueue<Token> pq = new PriorityQueue<>((a,b)-> a.value-b.value);
+            pq.add(new Token(0,0,map[0][0]));
 
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    dist[i][j] = Integer.MAX_VALUE;
+            while(!pq.isEmpty()){
+                Token current = pq.poll();
+                int cy = current.y;
+                int cx = current.x;
+                for(int[] direction: directions){
+                    int dy = direction[0];
+                    int dx = direction[1];
+                    int ny = cy+ dy;
+                    int nx = cx+ dx;
+                    if(nx<0||ny<0||ny>= map.length||nx>=map[0].length){
+                        continue;
+                    }
+
+                    if(costMap[ny][nx] > current.value + map[ny][nx]){
+                        costMap[ny][nx]  = current.value + map[ny][nx];
+                        pq.offer(new Token(ny,nx,costMap[ny][nx]));
+                    }
                 }
             }
-            dist[0][0] = 0;
-            dijkstra();
-            sb.append(dist[N-1][N-1]);
-            System.out.println(sb);
+            sb.append("Problem ").append(tc).append(": ").append(costMap[map.length-1][map.length-1]).append("\n");
         }
+        System.out.println(sb);
     }
 
-    private static void dijkstra(){
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.rupee - o2.rupee);
-        pq.add(new Node(0, 0, cave[0][0]));
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-            for(int k = 0; k < 4; k++) {
-                int nx = cur.vx + dx[k];
-                int ny = cur.vy + dy[k];
-                if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-                if(cur.rupee + cave[nx][ny] < dist[nx][ny]) {
-                    dist[nx][ny] = cur.rupee + cave[nx][ny];
-                    pq.offer(new Node(nx, ny, dist[nx][ny]));
-                }
-            }
+    public static class Token{
+        int y;
+        int x;
+        int value;
+        Token(int y,int x,int value){
+            this.y = y;
+            this.x = x;
+            this.value = value;
         }
     }
 }
