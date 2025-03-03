1b1927f203c2632ae00b55482bf6c61f2dc57589
YHK <42041887+ghrbs0437@users.noreply.github.com>
2025-03-03 13:28:28 +0900

    [Gold IV] Title: 비밀번호 만들기, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/17218. 비밀번호 만들기/비밀번호 만들기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+
+        String ans = getAns(str1, str2);
+        System.out.println(ans);
+
+    }
+
+    public static String getAns(String s1 ,String s2){
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder lcs = new StringBuilder();
+        int i = len1, j = len2;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString();
+    }
+}
