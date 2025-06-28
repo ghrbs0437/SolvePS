7332e5a06f45b6fce4a57451423e73db2d5deb00
YHK <42041887+ghrbs0437@users.noreply.github.com>
2025-06-29 01:20:39 +0900

    [Gold IV] Title: 회사 문화 1, Time: 624 ms, Memory: 89668 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -7,13 +7,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         String[] strs = br.readLine().split(" ");
         int n = Integer.parseInt(strs[0]); // 직원의 수
         int m = Integer.parseInt(strs[1]); // 최초의 칭찬 횟수
-        
+
         ArrayList<Node> nodes = new ArrayList<>();
-        
+
         for(int i=0; i<n;i++){
             Node node = new Node();
             nodes.add(node);
@@ -41,23 +41,16 @@ public class Main {
         nodes.get(root).praise(0);
 
         for(Node node : nodes){
-            System.out.print(node.count+" ");
+            sb.append(node.count).append(" ");
         }
+        System.out.println(sb);
 
     }
 
     public static class Node{
         int parent = -1;
-
         int count = 0;
         ArrayList<Node> children = new ArrayList<>();
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "parent=" + parent +
-                    '}';
-        }
 
         public void praise(int num){
             count+=num;
